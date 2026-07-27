a600afe66fd7fea21ccc53e428ded0ec051dbad6
yamunav3 <yamunav32006@gmail.com>
2026-07-27 20:11:33 +0530

    resolve 400 Bad Request in signup

--- src/main/java/com/example/Alumni_Backend/DTO/SignupRequest.java
+++ src/main/java/com/example/Alumni_Backend/DTO/SignupRequest.java
@@ -1,122 +1,84 @@
+
 package com.example.Alumni_Backend.DTO;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // Handles extra fields like "section"
 public class SignupRequest {
     private String username;
     private String password;
     private String email;
+
+    // This allows Jackson to accept "collegeID", "collegeid", or "collegeId"
+    @JsonProperty("collegeID")
+    @JsonAlias({"collegeid", "collegeId"})
     private String collegeID;
-    private long mobilenumber;
+
+
+    // This allows Jackson to accept "linkedin" or "linkedIn"
+    @JsonProperty("linkedin")
+    @JsonAlias("linkedIn")
+    private String linkedIn;
+
+
+
+
+
+    private String mobilenumber;
     private String interests;
     private String yearofpassing;
     private String workingcompany;
     private String jobrole;
-
     private String github;
-    private String linkedIn;
+
+
     private String fullname;
     private String branch;
+    private String section;
 
-    public String getGithub() {
-        return github;
-    }
+    // --- Getters and Setters ---
 
-    public void setGithub(String github) {
-        this.github = github;
-    }
+    public String getSection() { return section; }
+    public void setSection(String section) { this.section = section; }
 
-    public String getLinkedIn() {
-        return linkedIn;
-    }
+    public String getMobilenumber() { return mobilenumber; }
+    public void setMobilenumber(String mobilenumber) { this.mobilenumber = mobilenumber; }
 
-    public void setLinkedIn(String linkedIn) {
-        this.linkedIn = linkedIn;
-    }
+    public String getLinkedIn() { return linkedIn; }
+    public void setLinkedIn(String linkedIn) { this.linkedIn = linkedIn; }
 
-    public String getBranch() {
-        return branch;
-    }
+    public String getGithub() { return github; }
+    public void setGithub(String github) { this.github = github; }
 
-    public void setBranch(String branch) {
-        this.branch = branch;
-    }
+    public String getBranch() { return branch; }
+    public void setBranch(String branch) { this.branch = branch; }
 
-    public String getFullname() {
-        return fullname;
-    }
+    public String getFullname() { return fullname; }
+    public void setFullname(String fullname) { this.fullname = fullname; }
 
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getCollegeID() { return collegeID; }
+    public void setCollegeID(String collegeID) { this.collegeID = collegeID; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getInterests() { return interests; }
+    public void setInterests(String interests) { this.interests = interests; }
 
-    public String getCollegeID() {
-        return collegeID;
-    }
+    public String getYearofpassing() { return yearofpassing; }
+    public void setYearofpassing(String yearofpassing) { this.yearofpassing = yearofpassing; }
 
-    public void setCollegeID(String collegeID) {
-        this.collegeID = collegeID;
-    }
+    public String getWorkingcompany() { return workingcompany; }
+    public void setWorkingcompany(String workingcompany) { this.workingcompany = workingcompany; }
 
-    public long getMobilenumber() {
-        return mobilenumber;
-    }
+    public String getJobrole() { return jobrole; }
+    public void setJobrole(String jobrole) { this.jobrole = jobrole; }
 
-    public void setMobilenumber(long mobilenumber) {
-        this.mobilenumber = mobilenumber;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getInterests() {
-        return interests;
-    }
-
-    public void setInterests(String interests) {
-        this.interests = interests;
-    }
-
-    public String getYearofpassing() {
-        return yearofpassing;
-    }
-
-    public void setYearofpassing(String yearofpassing) {
-        this.yearofpassing = yearofpassing;
-    }
-
-    public String getWorkingcompany() {
-        return workingcompany;
-    }
-
-    public void setWorkingcompany(String workingcompany) {
-        this.workingcompany = workingcompany;
-    }
-
-    public String getJobrole() {
-        return jobrole;
-    }
-
-    public void setJobrole(String jobrole) {
-        this.jobrole = jobrole;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
